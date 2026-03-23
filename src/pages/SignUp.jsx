83ce8453f81aad35e487d47be7f0f20fd1362b02
unknown <shiclarecenon@gmail.com>
2026-03-23 10:00:47 +0800

    add css

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,34 +3,41 @@ import MainLayout from '../layouts/MainLayout';
 
 const SignUpPage = () => {
     return (
-        <MainLayout>
-            <Input
-                name="firstname"
-                placeholder="Enter your First Name"
-                label="First Name"
-                type="text"
-            />
-            <Input
-                name="lastname"
-                placeholder="Enter your Last Name"
-                label="Last Name"
-                type="text"
-            />
-            <Input
-                name="email"
-                placeholder="Enter your Email"
-                label="Email"
-                type="email"
-            />
-            <Input
-                name="password"
-                placeholder="Enter your Password"
-                label="Password"
-                type="password"
-            />
+        <div className="flex flex-col min-h-screen">
+            <MainLayout>
 
-        </MainLayout>
+                <div className="flex flex-1 items-center justify-center p-4">
+                    <div className="w-full max-w-md">
+                        <Input
+                            name="firstname"
+                            placeholder="Enter your First Name"
+                            label="First Name"
+                            type="text"
+                        />
+                        <Input
+                            name="lastname"
+                            placeholder="Enter your Last Name"
+                            label="Last Name"
+                            type="text"
+                        />
+                        <Input
+                            name="email"
+                            placeholder="Enter your Email"
+                            label="Email"
+                            type="email"
+                        />
+                        <Input
+                            name="password"
+                            placeholder="Enter your Password"
+                            label="Password"
+                            type="password"
+                        />
+
+                    </div>
+                </div>
+            </MainLayout>
+        </div>
     );
 };
 
-export default SignUpPage
+export default SignUpPage;
