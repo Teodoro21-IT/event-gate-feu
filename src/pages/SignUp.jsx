c4004982adc6290e08a28c62fc482330be76e7d8
unknown <shiclarecenon@gmail.com>
2026-03-23 09:26:30 +0800

    added sign up

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,36 @@
-import MainLayout from "../layouts/MainLayout";
-const SignUp = () => {
+import Input from '../components/form/Input';
+import MainLayout from '../layouts/MainLayout';
+
+const SignUpPage = () => {
     return (
         <MainLayout>
-            <div></div>
+            <Input
+                name="firstname"
+                placeholder="Enter your First Name"
+                label="First Name"
+                type="text"
+            />
+            <Input
+                name="lastname"
+                placeholder="Enter your Last Name"
+                label="Last Name"
+                type="text"
+            />
+            <Input
+                name="email"
+                placeholder="Enter your Email"
+                label="Email"
+                type="email"
+            />
+            <Input
+                name="password"
+                placeholder="Enter your Password"
+                label="Password"
+                type="password"
+            />
+
         </MainLayout>
-    )
+    );
 };
 
-export default SignUp
+export default SignUpPage
